4373d5b24a4050a20053f46f4bdcb183c270d6e8
MeshwaS <80071786+MeshwaS@users.noreply.github.com>
2021-03-08 16:38:37 -0500

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,8 +1,8 @@
 var typed = new Typed("#typing", {
   strings: [
     '" Provide the best equipments and solutions to enable our customers to safely build a better and stronger future."',
-    '" Building is about getting around the obstacles that are presented to you"',
-    '" Get stuff done"',
+    '" Building is about getting around the obstacles that are presented to you."',
+    '" Get stuff done."',
   ],
   loop: true,
   showCursor: false,
